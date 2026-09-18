5ae0c0c88259b30eff44fbaeb16b1b96238a159a
Obinna Okoro <obiraza@gmail.com>
2026-09-18 17:49:21 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const draftRoutes = require("./routes/draftRoute");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  }),
+);
+
 app.use(express.json());
 app.use("/upload", express.static("upload"));
 
